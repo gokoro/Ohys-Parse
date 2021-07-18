a74ea7f6058a4232b70eceb62ba1db8056526f73
Gokoro <lincroe@gmail.com>
2021-07-18 16:33:09 +0900

    Add handling exception in fetching anilist

--- src/services/anilist/fetch.js
+++ src/services/anilist/fetch.js
@@ -3,88 +3,92 @@ const axios = require('axios').default
 const logger = require('../../loaders/logger')
 
 module.exports = async (animeTitle) => {
-  if (!animeTitle) {
-    logger.error("the title for fetching anilist doesn't exists.")
-
-    return nullForm(animeTitle)
-  }
-  let response = null
-
-  logger.debug('Fetching anilist: ' + animeTitle)
-
-  response = await fetch(animeTitle)
-
-  // If infomations don't exist on Anilist... Handle Exceptions
-  if (response.data.hasOwnProperty('errors')) {
-    logger.debug('Fetching anilist was failed. Try again...')
-
-    // If the title wasn't splited..
-    if (animeTitle.indexOf('[Ohys-Raws]') > -1) {
-      logger.error('Trying failed. Reason: Received title is wrong.')
+  try {
+    if (!animeTitle) {
+      logger.error("the title for fetching anilist doesn't exists.")
 
       return nullForm(animeTitle)
     }
+    let response = null
 
-    const titleForHandleException = animeTitle.split(' ')
+    logger.debug('Fetching anilist: ' + animeTitle)
 
-    while (true) {
-      titleForHandleException.pop()
+    response = await fetch(animeTitle)
 
-      const splitedTitle = [...titleForHandleException].join(' ')
+    // If infomations don't exist on Anilist... Handle Exceptions
+    if (response.data.hasOwnProperty('errors')) {
+      logger.debug('Fetching anilist was failed. Try again...')
 
-      logger.debug('Retrieving for anilist with title: ' + splitedTitle)
+      // If the title wasn't splited..
+      if (animeTitle.indexOf('[Ohys-Raws]') > -1) {
+        logger.error('Trying failed. Reason: Received title is wrong.')
 
-      response = await fetch(splitedTitle)
-
-      if (!response.data.hasOwnProperty('errors')) {
-        logger.debug('Retrieving successed!')
-        break
-      }
-
-      if (titleForHandleException.length === 0) {
         return nullForm(animeTitle)
       }
+
+      const titleForHandleException = animeTitle.split(' ')
+
+      while (true) {
+        titleForHandleException.pop()
+
+        const splitedTitle = [...titleForHandleException].join(' ')
+
+        logger.debug('Retrieving for anilist with title: ' + splitedTitle)
+
+        response = await fetch(splitedTitle)
+
+        if (!response.data.hasOwnProperty('errors')) {
+          logger.debug('Retrieving successed!')
+          break
+        }
+
+        if (titleForHandleException.length === 0) {
+          return nullForm(animeTitle)
+        }
+      }
     }
-  }
 
-  // 'x-ratelimit-remaining' is zero, so Can't fetch anymore.
-  if (!Number(response.headers['x-ratelimit-remaining'])) {
-    // Sleep due to request limits of the APIs
+    // 'x-ratelimit-remaining' is zero, so Can't fetch anymore.
+    if (!Number(response.headers['x-ratelimit-remaining'])) {
+      // Sleep due to request limits of the APIs
 
-    const toWaitTime = Number(response.headers['x-ratelimit-limit']) + 1
+      const toWaitTime = Number(response.headers['x-ratelimit-limit']) + 1
 
-    logger.debug(
-      `Requesting Anilist is limited now. Please waiting for ${toWaitTime} seconds...`
-    )
+      logger.info(
+        `Requesting Anilist is limited now. Please waiting for ${toWaitTime} seconds...`
+      )
 
-    await new Promise((r) => setTimeout(r, toWaitTime * 1000))
+      await new Promise((r) => setTimeout(r, toWaitTime * 1000))
 
-    // Fetch again
-    logger.debug('Fetch anilist again...')
-    response = await fetch(animeTitle)
-  }
-  logger.silly('Anilist response: ' + JSON.stringify(response.data))
+      // Fetch again
+      logger.debug('Fetch anilist again...')
+      response = await fetch(animeTitle)
+    }
+    logger.silly('Anilist response: ' + JSON.stringify(response.data))
 
-  const {
-    data: { Media },
-  } = response.data
+    const {
+      data: { Media },
+    } = response.data
 
-  return {
-    id: Media.id,
-    name: animeTitle,
-    title: {
-      romaji: Media.title.romaji,
-      japanese: Media.title.native,
-      english: Media.title.english,
-    },
-    description: (Media.description || '').replace(/<[^>]*>/g, ''),
-    imageUrl: Media.coverImage.extraLarge,
-    smallImageUrl: Media.coverImage.large,
-    bannerImage: Media.bannerImage,
-    color: Media.coverImage.color,
-    season: Media.season,
-    released_year: Media.seasonYear,
-    episode_info: Media.streamingEpisodes,
+    return {
+      id: Media.id,
+      name: animeTitle,
+      title: {
+        romaji: Media.title.romaji,
+        japanese: Media.title.native,
+        english: Media.title.english,
+      },
+      description: (Media.description || '').replace(/<[^>]*>/g, ''),
+      imageUrl: Media.coverImage.extraLarge,
+      smallImageUrl: Media.coverImage.large,
+      bannerImage: Media.bannerImage,
+      color: Media.coverImage.color,
+      season: Media.season,
+      released_year: Media.seasonYear,
+      episode_info: Media.streamingEpisodes,
+    }
+  } catch (error) {
+    return nullForm()
   }
 }
 
