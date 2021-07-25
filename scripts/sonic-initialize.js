e9f9ce27105f0214021b269077c711e82102bc6b
Gokoro <lincroe@gmail.com>
2021-07-25 23:12:38 +0900

    Fix validating

--- scripts/sonic-initialize.js
+++ scripts/sonic-initialize.js
@@ -2,19 +2,26 @@
 
 const mongooseLoader = require('../src/loaders/mongoose')
 const sonic = require('../src/services/sonic')
+const logger = require('../src/loaders/logger')
 
 const Anime = require('../src/models/animes')
 
 mongooseLoader().then(async () => {
   for (let i = 1; true; i++) {
-    const fullTitleList = await Anime.find({}, ['title', '_id'])
+    logger.info(`Processing ${i} page...`)
+
+    const fullTitleList = await Anime.find({}, ['name', 'title', '_id'])
       .skip((i - 1) * 100)
       .limit(100)
 
+    if (fullTitleList.length === 0) {
+      return
+    }
+
     for await (const item of fullTitleList) {
       // Title check for searching
-      if (!sonic.isValid(item.name)) {
-        return
+      if (!sonic.isValid(item.name) || !sonic.isValid(item.title.romaji)) {
+        continue
       }
 
       const promiseList = [
